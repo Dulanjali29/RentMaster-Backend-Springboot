a61033c02f1b261178909ea4f7685b8a764d430d
Dulanjali <your-github-email@example.com>
2026-08-29 09:59:19 +0530

    first commit

---
+++ src/main/java/lk/wusl/rentmaster/RentmasterApplication.java
@@ -0,0 +1,14 @@
+package lk.wusl.rentmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentmasterApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RentmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/wusl/rentmaster/RentmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.wusl.rentmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
